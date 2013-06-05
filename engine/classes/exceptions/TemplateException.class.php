9aa147f8830f279432c0608ddd84e5ebda893c53
Marcus Povey <marcus@marcus-povey.co.uk>
2013-06-05 17:45:41 +0100

    Template exception

---
+++ engine/classes/exceptions/TemplateException.class.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @file
+ * Template exception.
+ * 
+ * @package core
+ * @copyright Marcus Povey 2013
+ * @license $$LICENCE$$
+ * @author Marcus Povey <marcus@marcus-povey.co.uk>
+ * @link http://www.marcus-povey.co.uk
+ */
+
+namespace home_io\core\exceptions {
+
+    class TemplateException extends HomeException {
+        
+    }
+
+}
