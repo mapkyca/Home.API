8a1c958cbb660192902c327ceb6e47a8da36ae28
Marcus Povey <marcus@marcus-povey.co.uk>
2013-06-05 17:37:08 +0100

    Error handler

--- engine/classes/Errors.class.php
+++ engine/classes/Errors.class.php
@@ -11,112 +11,118 @@
  * @link http://www.marcus-povey.co.uk
  */
 
-/**
- * Error handling functions.
- */
-class Errors {
+namespace home_io\core {
 
     /**
-     * Trap PHP error message.
-     * 
-     * @see http://www.php.net/set-error-handler
-     * @param int $errno The level of the error raised
-     * @param string $errmsg The error message
-     * @param string $filename The filename the error was raised in
-     * @param int $linenum The line number the error was raised at
-     * @param array $vars An array that points to the active symbol table at the point that the error occurred
+     * Error handling functions.
      */
-    public static function __error_handler($errno, $errmsg, $filename, $linenum, $vars) {
-        
-        // Lazy loader sometimes doesn't handle errors too well, so load stats classes if not already present.
-        if (!class_exists('Stats')) require_once(dirname(__FILE__) . '/Stats.class.php');
-        if (!class_exists('EtsyStatsdStats')) require_once(dirname(__FILE__) . '/EtsyStatsdStats.class.php');
-        
-        $error = date("Y-m-d H:i:s (T)") . ": \"" . $errmsg . "\" in file " . $filename . " (line " . $linenum . ")";
+    class Errors {
 
-        try {
-            Stats::i('error.total');
+        /**
+         * Trap PHP error message.
+         * 
+         * @see http://www.php.net/set-error-handler
+         * @param int $errno The level of the error raised
+         * @param string $errmsg The error message
+         * @param string $filename The filename the error was raised in
+         * @param int $linenum The line number the error was raised at
+         * @param array $vars An array that points to the active symbol table at the point that the error occurred
+         */
+        public static function __error_handler($errno, $errmsg, $filename, $linenum, $vars) {
 
-            switch ($errno) {
-                case E_USER_ERROR:
-                    Stats::i('error.error');
-                    Site::error($error);
-                    break;
+            // Lazy loader sometimes doesn't handle errors too well, so load stats classes if not already present.
+            if (!class_exists('Stats'))
+                require_once(dirname(__FILE__) . '/Stats.class.php');
+            if (!class_exists('EtsyStatsdStats'))
+                require_once(dirname(__FILE__) . '/EtsyStatsdStats.class.php');
 
-                case E_WARNING :
-                case E_USER_WARNING :
-                    Stats::i('error.warning');
-                    Site::warning($error);
-                    break;
+            $error = date("Y-m-d H:i:s (T)") . ": \"" . $errmsg . "\" in file " . $filename . " (line " . $linenum . ")";
 
-                default:
-                    Stats::i('error.debug');
-                    Site::log_echo($error, 'DEBUG');
+            try {
+                Stats::i('error.total');
+
+                switch ($errno) {
+                    case E_USER_ERROR:
+                        Stats::i('error.error');
+                        Site::error($error);
+                        break;
+
+                    case E_WARNING :
+                    case E_USER_WARNING :
+                        Stats::i('error.warning');
+                        Site::warning($error);
+                        break;
+
+                    default:
+                        Stats::i('error.debug');
+                        Site::log_echo($error, 'DEBUG');
+                }
+            } catch (Exception $e) {
+                die($e->getMessage());
             }
-        } catch (Exception $e) {
-            die($e->getMessage());
+
+
+            return true;
         }
 
+        /**
+         * Custom exception handler.
+         * This function catches any thrown exceptions and handles them appropriately.
+         *
+         * @see http://www.php.net/set-exception-handler
+         * @param Exception $exception The exception being handled
+         */
+        public static function __exception_handler($exception) {
 
-        return true;
-    }
+            // Lazy loader sometimes doesn't handle errors too well, so load stats classes if not already present.
+            if (!class_exists('Stats'))
+                require_once(dirname(__FILE__) . '/Stats.class.php');
 
-    /**
-     * Custom exception handler.
-     * This function catches any thrown exceptions and handles them appropriately.
-     *
-     * @see http://www.php.net/set-exception-handler
-     * @param Exception $exception The exception being handled
-     */
-    public static function __exception_handler($exception) {
+            // Log exception (and we can't have any exceptions)
+            try {
+                Stats::i('exception.total');
+                Stats::i('exception.' . strtolower(get_class($exception)));
+            } catch (Exception $e) {
+                die($e->getMessage());
+            }
 
-        // Lazy loader sometimes doesn't handle errors too well, so load stats classes if not already present.
-        if (!class_exists('Stats')) require_once(dirname(__FILE__) . '/Stats.class.php');
-        
-        // Log exception (and we can't have any exceptions)
-        try {
-            Stats::i('exception.total');
-            Stats::i('exception.' . strtolower(get_class($exception)));
-        } catch (Exception $e) {
-            die($e->getMessage());
+            ob_end_clean(); // Clear existing / half empty buffer
+            // Log exception
+            Site::log_echo($exception->getMessage(), 'EXCEPTION');
+
+            // Optionally email exception
+            if (Site::$config->email_exceptions == true) {
+                mail(
+                        Site::$config->admin_email, 'EXCEPTION ' . Site::$config->name . " : " . $exception->getMessage(), $exception->getTraceAsString()
+                );
+            }
+
+            // If this is a platform exception then render it creatively, otherwise enforce the default
+            if ($exception instanceof SiteException)
+                $body = "$exception";
+            else
+                $body = Template::v('exceptions/__default', array('exception' => $exception));
+
+            Template::getInstance()->outputPage(i18n::w('exception:title'), $body);
         }
 
-        ob_end_clean(); // Clear existing / half empty buffer
-        // Log exception
-        Site::log_echo($exception->getMessage(), 'EXCEPTION');
+        public static function init() {
+            // Now set php error handlers
+            if (Site::$config->debug)
+                set_error_handler('Errors::__error_handler', E_ALL & E_STRICT);
+            else
+                set_error_handler('Errors::__error_handler', E_ALL & ~E_NOTICE); // Hide notice level errors when not in debug
 
-        // Optionally email exception
-        if (Site::$config->email_exceptions == true) {
-            mail(
-                    Site::$config->admin_email, 'EXCEPTION ' . Site::$config->name . " : " . $exception->getMessage(), $exception->getTraceAsString()
-            );
+            set_exception_handler('Errors::__exception_handler');
+
+            // Class loader for exceptions
+            spl_autoload_register(function($class) {
+                        $file = dirname(__FILE__) . '/exceptions/' . $class . '.class.php';
+                        if (file_exists($file))
+                            include($file);
+                    });
         }
 
-        // If this is a platform exception then render it creatively, otherwise enforce the default
-        if ($exception instanceof SiteException)
-            $body = "$exception";
-        else
-            $body = Template::v('exceptions/__default', array('exception' => $exception));
-
-        Template::getInstance()->outputPage(i18n::w('exception:title'), $body);
-    }
-
-    public static function init() {
-        // Now set php error handlers
-        if (Site::$config->debug)
-            set_error_handler('Errors::__error_handler', E_ALL & E_STRICT);
-        else
-            set_error_handler('Errors::__error_handler', E_ALL & ~E_NOTICE); // Hide notice level errors when not in debug
-
-        set_exception_handler('Errors::__exception_handler');
-        
-        // Class loader for exceptions
-        spl_autoload_register(function($class) {
-            $file = dirname(__FILE__) . '/exceptions/' . $class . '.class.php';
-            if (file_exists($file))
-                include($file);
-        });
     }
 
 }
-
