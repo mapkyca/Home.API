ce6e1dfa152281efe7b6b0ea1bb29204e45d5554
Marcus Povey <marcus@marcus-povey.co.uk>
2013-06-05 18:34:36 +0100

    Translation exception

---
+++ engine/classes/home_io/core/exceptions/i18nException.class.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @file
+ * Internationalisation exception
+ * 
+ * @package core
+ * @copyright Marcus Povey 2013
+ * @license $$LICENCE$$
+ * @author Marcus Povey <marcus@marcus-povey.co.uk>
+ * @link http://www.marcus-povey.co.uk
+ */
+
+namespace home_io\core\exceptions {
+
+
+    class i18nException extends HomeException {
+        
+    }
+
+}
