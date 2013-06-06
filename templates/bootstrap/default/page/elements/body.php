8a5ddd1a2325e69705e4107fd90ade43747f9a8c
Marcus Povey <marcus@marcus-povey.co.uk>
2013-06-06 09:03:34 +0100

    Removing unnecessary lines

--- templates/bootstrap/default/page/elements/body.php
+++ templates/bootstrap/default/page/elements/body.php
@@ -2,18 +2,12 @@
 	<?php echo \home_io\templates\Template::v('page/elements/messages'); ?>
 	
 		    <?php echo \home_io\templates\Template::v('page/elements/pagetop', $vars); ?>
-		    <?php
-        if ($vars['no_container_wrap'])
-            echo $vars['body'];
-        else
-        {
-            ?>
+
         <div id="content">
             <div class="container">	
                 <?php echo $vars['body']; ?>
             </div>
         </div>
-        <?php } ?>
 		    <?php echo \home_io\templates\Template::v('page/elements/pagebottom', $vars); ?>
 	
     
