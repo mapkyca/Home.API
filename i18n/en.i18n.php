59ca33609b4a838236ad8833281d900e05f9cbe1
Marcus Povey <marcus@marcus-povey.co.uk>
2013-06-06 20:19:45 +0100

    API Not found exception

--- i18n/en.i18n.php
+++ i18n/en.i18n.php
@@ -9,7 +9,8 @@ namespace home_io\i18n {
         'page:exception:notfound' => 'Page \'%s\' not found.',
         // API
         'api:exception:class_not_specified' => 'Endpoint definition %s has no class entry defined!',
-        'api:exception:class_not_found' => 'Class %s could not be found.'
+        'api:exception:class_not_found' => 'Class %s could not be found.',
+        'api:exception:api_not_found' => 'The requested API (\'%s\') was not found on this server, please check your definition file.'
     );
 
     Basic::register($errors, 'en');
