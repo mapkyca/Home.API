bced3fa4920c5d4448563b68c86a0bfe326d647b
Marcus Povey <marcus@marcus-povey.co.uk>
2013-06-10 17:02:29 +0100

    Back to top

--- templates/home.io/default/page/elements/pagebottom.php
+++ templates/home.io/default/page/elements/pagebottom.php
@@ -3,7 +3,7 @@
     <div class="container">
         <div class="row">
             <div class="span12">
-                <p>&copy; 2013 <a href="http://www.marcus-povey.co.uk">Marcus Povey</a> <span id="totop" class="pull-right">Back to Top <i class="icon-arrow-up"></i></span></p>			
+                <p>&copy; 2013 <a href="http://www.marcus-povey.co.uk">Marcus Povey</a> <span id="totop" class="pull-right">Back to Top <a href="#"><i class="icon-arrow-up"></i></a></span></p>			
             </div>		  
         </div>	  
     </div>	  
