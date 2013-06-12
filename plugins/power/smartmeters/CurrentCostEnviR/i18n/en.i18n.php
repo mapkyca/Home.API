74d0d288135c2893ff4727b323f67286ad6a69b0
Marcus Povey <marcus@marcus-povey.co.uk>
2013-06-12 13:09:52 +0100

    Translations

--- plugins/power/smartmeters/CurrentCostEnviR/i18n/en.i18n.php
+++ plugins/power/smartmeters/CurrentCostEnviR/i18n/en.i18n.php
@@ -7,8 +7,13 @@ namespace power\smartmeters {
         'currentcostenvir:exception:temp_unavailable' => 'Temperature currently not available.',
         'currentcostenvir:exception:power_unavailable' => 'Power not currently available.',
         
-        'currentcostenvir:exception:cant_open_device' => "Can't open device (%s:%d), please make sure that the port is readable by your web server user."
+        'currentcostenvir:exception:cant_open_device' => "Can't open device (%s:%d), please make sure that the port is readable by your web server user.",
         
+        'currentcostenvir:time' => 'Time',
+        'currentcostenvir:temp' => 'Temperature',
+        'currentcostenvir:temp:reading' => '%s C',
+        'currentcostenvir:power' => 'Reading',
+        'currentcostenvir:power:reading' => '%s watts',
     );
 
     \home_api\i18n\Basic::register($en, 'en');
