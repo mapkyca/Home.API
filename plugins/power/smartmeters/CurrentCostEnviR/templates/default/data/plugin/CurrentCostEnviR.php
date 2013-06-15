b800e17babb10915eb9c7466c426a28443ba3747
Marcus Povey <marcus@marcus-povey.co.uk>
2013-06-15 19:14:57 +0100

    Tidy up

--- plugins/power/smartmeters/CurrentCostEnviR/templates/default/data/plugin/CurrentCostEnviR.php
+++ plugins/power/smartmeters/CurrentCostEnviR/templates/default/data/plugin/CurrentCostEnviR.php
@@ -1,33 +1,6 @@
 <?php
 $object = $vars['object'];
 
-$details = array();
-/*
-$details['time_ok'] = true;
-$details['temp_ok'] = true;
-$details['power_ok'] = true;
-
-try {
-    $details['time_value'] = $object->time();
-} catch (Exception $e) {
-    $details['time_ok'] = false;
-    $details['time_value'] = $e->getMessage();
-}
-
-try {
-    $details['temp_value'] = home_api\i18n\i18n::w('currentcostenvir:temp:reading', array($object->temp()));
-} catch (Exception $e) {
-    $details['temp_ok'] = false;
-    $details['temp_value'] = $e->getMessage();
-}
-
-try {
-    $details['power_value'] = home_api\i18n\i18n::w('currentcostenvir:power:reading', array($object->power()));
-} catch (Exception $e) {
-    $details['power_ok'] = false;
-    $details['power_value'] = $e->getMessage();
-}
-*/
 ?>
 <div id="plugin-for-<?= str_replace('/', '-', $vars['call']); ?>" class="well plugin CurrentCostEnviR">
     <h2><?= $vars['call']; ?></h2>
