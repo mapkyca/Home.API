936b6a31f9b351e57f067da14a80f21a5c0bdf41
Marcus Povey <marcus@marcus-povey.co.uk>
2013-06-15 18:43:55 +0100

    Current cost envir display now using JS API

--- plugins/power/smartmeters/CurrentCostEnviR/templates/default/data/plugin/CurrentCostEnviR.php
+++ plugins/power/smartmeters/CurrentCostEnviR/templates/default/data/plugin/CurrentCostEnviR.php
@@ -2,7 +2,7 @@
 $object = $vars['object'];
 
 $details = array();
-
+/*
 $details['time_ok'] = true;
 $details['temp_ok'] = true;
 $details['power_ok'] = true;
@@ -27,25 +27,69 @@ try {
     $details['power_ok'] = false;
     $details['power_value'] = $e->getMessage();
 }
-
+*/
 ?>
 <div id="plugin-for-<?= str_replace('/', '-', $vars['call']); ?>" class="well plugin CurrentCostEnviR">
     <h2><?= $vars['call']; ?></h2>
-    <table class="table table-striped table-bordered">
+    <div class="progress progress-striped active">
+        <div class="bar" style="width: 100%;"></div>
+    </div>
+    <table class="table table-striped table-bordered" style="display: none;">
             
-            <tr class="<?= $details['time_ok'] ? 'success' : 'error'; ?>">
+            <tr class="time">
                 <td><b><?= home_api\i18n\i18n::w('currentcostenvir:time'); ?></b></td>
-                <td><?= $details['time_value'];?></td>
+                <td class="reading"></td>
             </tr>
             
-            <tr class="<?= $details['temp_ok'] ? 'success' : 'error'; ?>">
+            <tr class="temp">
                 <td><b><?= home_api\i18n\i18n::w('currentcostenvir:temp'); ?></b></td>
-                <td><?= $details['temp_value'];?></td>
+                <td class="reading"></td>
             </tr>
-            <tr class="<?= $details['power_ok'] ? 'success' : 'error'; ?>">
+            <tr class="power">
                 <td><b><?= home_api\i18n\i18n::w('currentcostenvir:power'); ?></b></td>
-                <td><?= $details['power_value'];?></td>
+                <td class="reading"></td>
             </tr>
             
     </table>
 </div>
+<script>
+    $(document).ready(function(){
+        HomeAPI.call('<?=$vars['call'];?>/latest.json', {}, function(result) {
+            HomeAPI.extractBody(result, function(data) {
+                
+                $('#plugin-for-<?= str_replace('/', '-', $vars['call']); ?> div.progress').fadeOut();
+                $('#plugin-for-<?= str_replace('/', '-', $vars['call']); ?> table').fadeIn();
+                
+                // Get time
+                if (data.time != null) {
+                    $('#plugin-for-<?= str_replace('/', '-', $vars['call']); ?> tr.time').addClass('info');
+                    $('#plugin-for-<?= str_replace('/', '-', $vars['call']); ?> tr.time td.reading').html(data.time);
+                }
+                else {
+                    $('#plugin-for-<?= str_replace('/', '-', $vars['call']); ?> tr.time').addClass('error');
+                    $('#plugin-for-<?= str_replace('/', '-', $vars['call']); ?> tr.time td.reading').html("<?=\home_api\i18n\i18n::w('currentcostenvir:exception:time_unavailable')?>");
+                }
+                
+                // Temperature
+                if (data.temp != null) {
+                    $('#plugin-for-<?= str_replace('/', '-', $vars['call']); ?> tr.temp').addClass('info');
+                    $('#plugin-for-<?= str_replace('/', '-', $vars['call']); ?> tr.temp td.reading').html(data.temp + "C");
+                }
+                else {
+                    $('#plugin-for-<?= str_replace('/', '-', $vars['call']); ?> tr.temp').addClass('error');
+                    $('#plugin-for-<?= str_replace('/', '-', $vars['call']); ?> tr.temp td.reading').html("<?=\home_api\i18n\i18n::w('currentcostenvir:exception:temp_unavailable')?>");
+                }
+                
+                // Power
+                if (data.power != null) {
+                    $('#plugin-for-<?= str_replace('/', '-', $vars['call']); ?> tr.power').addClass('info');
+                    $('#plugin-for-<?= str_replace('/', '-', $vars['call']); ?> tr.power td.reading').html(data.power + " watts");
+                }
+                else {
+                    $('#plugin-for-<?= str_replace('/', '-', $vars['call']); ?> tr.power').addClass('error');
+                    $('#plugin-for-<?= str_replace('/', '-', $vars['call']); ?> tr.power td.reading').html("<?=\home_api\i18n\i18n::w('currentcostenvir:exception:power_unavailable')?>");
+                }
+            });
+        });
+    });
+</script>
