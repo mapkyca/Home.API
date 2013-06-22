f31e934f52ae35a5b97a25290b621f6cd9029656
Marcus Povey <marcus@marcus-povey.co.uk>
2013-06-22 15:31:06 +0100

    Lets not take people away from the site!

--- templates/Home.API/default/page/elements/pagebottom.php
+++ templates/Home.API/default/page/elements/pagebottom.php
@@ -3,7 +3,7 @@
     <div class="container">
         <div class="row">
             <div class="span12">
-                <p>&copy; 2013<?php $date = date('Y'); if ( $date != "2013") echo "-{$date}"; ?> <a href="http://www.marcus-povey.co.uk">Marcus Povey</a> <span id="totop" class="pull-right">Back to Top <a href="#"><i class="icon-arrow-up"></i></a></span></p>			
+                <p>&copy; 2013<?php $date = date('Y'); if ( $date != "2013") echo "-{$date}"; ?> <a href="http://www.marcus-povey.co.uk" target="_blank">Marcus Povey</a> <span id="totop" class="pull-right">Back to Top <a href="#"><i class="icon-arrow-up"></i></a></span></p>			
             </div>		  
         </div>	  
     </div>	  
